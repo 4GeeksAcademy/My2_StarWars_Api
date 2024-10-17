f00b651e432b0e23f8bf8a5f0aed48811805963d
Jumpvzl <40275118+Jumpvzl@users.noreply.github.com>
2024-10-17 04:40:40 +0000

    Se carga linea base

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import Login, Planet, Vehicle, Character, Favorite, FavoriteCharacter, FavoriteVehicle, FavoritePlanet, db, User
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -9,8 +9,15 @@ def setup_admin(app):
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
     
-    # Add your models here, for example this is how we add a the User model to the admin
+      # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
 
     # You can duplicate that line to add mew models
-    # admin.add_view(ModelView(YourModelName, db.session))
+    admin.add_view(ModelView(Planet, db.session))
+    admin.add_view(ModelView(Vehicle, db.session))
+    admin.add_view(ModelView(Character, db.session))
+    admin.add_view(ModelView(Favorite, db.session))
+    admin.add_view(ModelView(Login, db.session))
+    admin.add_view(ModelView(FavoriteCharacter, db.session))
+    admin.add_view(ModelView(FavoriteVehicle, db.session))
+    admin.add_view(ModelView(FavoritePlanet, db.session))
--- src/models.py
+++ src/models.py
@@ -2,11 +2,69 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Planet(db.Model):
+    __tablename__ = 'planets'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250))
+    gravity = db.Column(db.String(250))
+    population = db.Column(db.String(250))
+    climate = db.Column(db.String(250))
+
+class Vehicle(db.Model):
+    __tablename__ = 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    lastname = db.Column(db.String(250))
+    description = db.Column(db.String(250))
+    model = db.Column(db.String(250))
+    manufacturer = db.Column(db.String(250))
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250))
+    color_eyes = db.Column(db.String(250))
+    birth_year = db.Column(db.String(250))
+    gender = db.Column(db.String(6))
+    hair_color = db.Column(db.String(250))
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(250))
+    apellido = db.Column(db.String(250))
+    email = db.Column(db.String(250))
+    fecha_ingreso = db.Column(db.String(250))
+    is_active = db.Column(db.Boolean(), default=True, unique=False, nullable=False)
+
+
+class Login(db.Model):
+    __tablename__ = 'logins'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250))
+    password = db.Column(db.String(250))
+    usuario_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    favorites = db.Column(db.Enum('personaje', 'vehiculo', 'planeta', name='favorite_types'))
+
+class FavoriteCharacter(db.Model):
+    __tablename__ = 'favoritecharacters'
+    id = db.Column(db.Integer, primary_key=True)
+
+class FavoriteVehicle(db.Model):
+    __tablename__ = 'favoritevehicles'
+    id = db.Column(db.Integer, primary_key=True)
+
+class FavoritePlanet(db.Model):
+    __tablename__ = 'favoriteplanets'
+    id = db.Column(db.Integer, primary_key=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
